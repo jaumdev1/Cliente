03a09905df5c78c49c63dab9d4c946ed1819a1c1
joao-dev1 <joao280799@gmail.com>
2022-04-14 15:22:39 -0300

    primeiro commit Cliente

---
+++ Clientes/Classes/Cliente.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clientes.Classes
+{
+    public class Cliente
+    {
+        public Cliente(string nome, string telefone, string cpf)
+        {
+            this.Nome = nome;
+            this.Telefone = telefone;   
+            this.CPF = cpf;
+        }
+        public Cliente()
+        {
+            
+        }
+        public string Nome;
+        public string Telefone;
+        public string CPF;
+
+
+        public void Gravar()
+        {
+
+        }
+        private static string caminhoBasesClientes()
+        {
+            return ConfigurationManager.AppSettings["BaseDeClientes"];
+        }
+        public static List<Cliente> LerClientes()
+        {
+            var clientes = new List <Cliente>();
+
+            if (File.Exists(caminhoBasesClientes()))
+            {
+                using (StreamReader arquivo = File.OpenText(caminhoBasesClientes()))
+                {
+                    string linha;
+                    int i = 0;
+                    while ((linha = arquivo.ReadLine()) != null)
+                    {
+                        i++;
+                        if (i == 1) continue;                                           
+                        var clienteArquivo = linha.Split(';');
+                       // var cliente = new Cliente { Nome = clienteArquivo[0], Telefone= clienteArquivo[1], CPF  = clienteArquivo[2]};
+                        var cliente = new Cliente(clienteArquivo[0],clienteArquivo[1], clienteArquivo[2]);
+                        clientes.Add(cliente);
+
+                                   
+                    }
+                }
+            }
+            
+            return clientes;
+                       
+        }
+
+    }
+}
---
+++ Clientes/Program.cs
@@ -0,0 +1,27 @@
+﻿using Clientes.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clientes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var clientes = Cliente.LerClientes();
+            foreach(Cliente c in clientes)
+            {
+                Console.WriteLine(c.Nome);
+
+            }
+
+            Console.ReadLine();
+
+            
+        }
+    }
+}
